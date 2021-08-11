d81891b33243ddc582321bd4fbe62e86f30b8875
Bernhard <bniksch2@gmail.com>
2021-08-11 18:41:31 +0200

    Named URLs

--- roll_initiative/urls.py
+++ roll_initiative/urls.py
@@ -5,9 +5,9 @@ from schema_graph.views import Schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/character/', include('character.urls')),
-    path('api/equipment/', include('equipment.urls')),
-    path('api/monster/', include('monster.urls')),
+    path('api/character/', include('character.urls'), name="character"),
+    path('api/equipment/', include('equipment.urls'), name="equipment"),
+    path('api/monster/', include('monster.urls'), name="monster"),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema-graph/', Schema.as_view(), name='schema_graph'),
     path('schema-plate/', include('django_spaghetti.urls'), name='schema_plate'),
